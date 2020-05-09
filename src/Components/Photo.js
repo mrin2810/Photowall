5c6ff7978da0a346e87f37d78fcadf501340c568
mrinmayee.rane <mrinmayee.rane@zeuslearning.com>
2020-05-09 22:37:34 +0530

    Project Without Firebase

---
+++ src/Components/Photo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Stateless functions
+function Photo(props){
+    const post = props.post;
+    return <figure className="figure">
+                <img className="photo" src={post.imageLink} alt={post.description}/>
+                <figcaption><p>{post.description}</p></figcaption>
+                <div className="button-container">
+                <button onClick={() => {
+                        props.onRemovePhoto(post)
+                    }}>Remove</button>
+                </div>
+            </figure>
+}
+
+Photo.propTypes ={
+    post: PropTypes.object.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
+
+// class Photo extends Component{
+//     render(){
+//         const post = this.props.post;
+//     return <figure className="figure">
+//                 <img className="photo" src={post.imageLink} alt={post.description}/>
+//                 <figcaption><p>{post.description}</p></figcaption>
+//                 <div className="button-container">
+//                     <button className="button" onClick={() => {
+//                         this.props.onRemovePhoto(post)
+//                     }}>Remove</button>
+//                 </div>
+//             </figure>
+//     }
+// }
+
+export default Photo;
